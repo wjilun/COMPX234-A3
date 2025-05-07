6c4d3835dcb420deae9e720e4525e1de5922a2d1
wjilun <1879430899@qq.com>
2025-05-07 23:55:21 +0800

    initialization
    here I create a tuples to store tuple like (key:value),and I create a lock to protect shared resources (such as the self.tuples dictionary) in a multithreaded environment, preventing multiple threads from accessing and modifying simultaneously.and to meet the server request,on its side, displays every 10s a summary of the current tuple space, containing the number of tuples in the tuple space, the average tuple size, the average key size, and the average value size (string), the total number of clients which haveconnected (finished or not) so far, the total number of operations, total READs, total GETs, total PUTs, and how many errors

--- tuple_space.py
+++ tuple_space.py
@@ -1,2 +1,15 @@
 import threading
 
+class TupleSpace:
+    def __init__(self):
+        self.tuples={}
+        self.lock = threading.Lock()
+        self.number_of_tuples = 0
+        self.average_tuples_size = 0
+        self.average_key_size=0
+        self.average_value_size=0
+        self.connet_client_number=0
+        self.total_oprations=0
+        self.total_GET=0
+        self.total_PUT=0
+        self.total_READ=0
