a869ef55023c24e1eb66eaf7eaabf76a1831a253
wjilun <1879430899@qq.com>
2025-05-08 13:59:27 +0800

    get server's cmd value and key
    by finding the "" space ,and the cmd whether "P" ,I can get the key and the value.

--- server.py
+++ server.py
@@ -16,4 +16,24 @@ def print_stats(tuple_space):
 
 def handle_client(conn, addr, tuple_space):
     print(f"Connected by {addr}")
-    tuple_space.increment_client()        
+    tuple_space.increment_client()
+    try:
+         while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            data_str = data.decode('utf-8')
+            length_prefix = data_str[:3]
+            try:
+                length = int(length_prefix)
+                cmd = data_str[3]
+                key_end_idx = data_str.find(' ', 4)
+                if key_end_idx == -1:
+                    key = data_str[4:]
+                else:
+                    key=data_str[4:key_end_idx]
+                if cmd=="P":
+                    value = data_str[key_end_idx + 1:].strip()
+                else:
+                    value = None        
+                        
