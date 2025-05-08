018a08266051022ad632ba1f94908839075f218f
wjilun <1879430899@qq.com>
2025-05-08 11:27:49 +0800

    connect to server
    here I create a connection with server.

--- user_class.py
+++ user_class.py
@@ -17,3 +17,12 @@ class User:
             return f"{len(payload) + 7:03d}{payload}"
         else:
             raise ValueError("Invalid command")
+
+    def handle_client_file(self, filename, server_address):
+        with open(filename, 'r') as file:
+            client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            client_socket.connect(server_address)    
+
+
+
+     
