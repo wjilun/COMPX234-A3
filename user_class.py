c18b7667227aa3ec49bef1c6087d12091a20243e
wjilun <1879430899@qq.com>
2025-05-08 11:10:23 +0800

    encode request
    This method is used to encode requests based on the passed command and send It to server . Construct corresponding request strings according to different commands (PUT, GET, READ).For example,007Ra

--- user_class.py
+++ user_class.py
@@ -4,3 +4,16 @@ import re
 class User:
     def __init__(self):
         pass
+
+    def encode_request(self, cmd, key, value=None):
+        if cmd == 'PUT':
+            payload = f"P{key} {value}"
+            return f"{len(payload) + 6:03d}{payload}"
+        elif cmd == 'GET':
+            payload = f"G{key}"
+            return f"{len(payload) + 6:03d}{payload}"
+        elif cmd == 'READ':
+            payload = f"R{key}"
+            return f"{len(payload) + 7:03d}{payload}"
+        else:
+            raise ValueError("Invalid command")
