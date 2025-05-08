290cd32487b42afe4e659d076fa206c098ec3289
wjilun <1879430899@qq.com>
2025-05-08 15:45:23 +0800

    change
    change the key

--- user3.py
+++ user3.py
@@ -13,7 +13,7 @@ def main():
     e=user.READ("key1")
     print(e)
     print("read a value from our client_1 file's key")
-    f=user.READ("linkup")
+    f=user.READ("lobularia")
     print(f)
 
 
