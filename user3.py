1f756ad8e84312716437c6b277c1580e5713fbe0
wjilun <1879430899@qq.com>
2025-05-08 15:34:51 +0800

    create user3
    prove the tuples_space works ,and Prove that the tuples are interoperable and server and users are interoperable  .

---
+++ user3.py
@@ -0,0 +1,21 @@
+from user_class import User
+
+def main():
+    user = User()
+    a=user.PUT("key1", "value1")
+    print(a)
+    b=user.PUT("key1", "value2")
+    print(b)
+    c=user.READ("key1")
+    print(c)
+    d=user.GET("key1")
+    print(d)
+    e=user.READ("key1")
+    print(e)
+    print("read a value from our client_1 file's key")
+    f=user.READ("linkup")
+    print(f)
+
+
+if __name__ == "__main__":
+    main()    
